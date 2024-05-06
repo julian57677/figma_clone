16a9b2ccee04fa53fcda33443637c9cf2ce605e2
MRJULTZ\atsej <atsejulian@gmail.com>
2024-05-06 11:37:34 +0700

    fix page tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,58 +11,94 @@ import { ActiveElement } from "@/types/type";
 import { useMutation, useStorage } from "@/liveblocks.config";
 
 export default function Page() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const fabricRef = useRef<fabric.Canvas |null>(null);
-  const isDrawing = useRef(false);
-  const shapeRef = useRef<fabric.Object | null>(null);
-  const selectedShapeRef = useRef<string | null>(null);
-  
-  const activeObjectRef = useRef<fabric.Object | null>(null);
-  const canvasObjects = useStorage((root) => root.canvasObjects);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const fabricRef = useRef<fabric.Canvas |null>(null);
+  const isDrawing = useRef(false);
+  const shapeRef = useRef<fabric.Object | null>(null);
+  const selectedShapeRef = useRef<string | null>(null); 
+  const activeObjectRef = useRef<fabric.Object | null>(null);
+  const canvasObjects = useStorage((root) => root.canvasObjects);
 
-  const syncShapeInStorage = useMutation(({ storage }, object) => {
-    if(!object) return;
-    const{objectId} = object;
-    const shapeData = object.toJSON();
-    shapeData.objectId = objectId;
-    const canvasObjects = storage.get('canvasObjects');
-    canvasObjects.set(objectId, shapeData);
-  }, []);
+  const syncShapeInStorage = useMutation(({ storage }, object) => {
+    if(!object) return;
+    const{objectId} = object;
+    const shapeData = object.toJSON();
+    shapeData.objectId = objectId;
+    const canvasObjects = storage.get('canvasObjects');
+    canvasObjects.set(objectId, shapeData);
+  }, []);
 
-  const [activeElement, setActiveElement] = useState<ActiveElement>({
-    name: '',
-    value: '',
-    icon: ''
-  })
+  const [activeElement, setActiveElement] = useState<ActiveElement>({
+    name: '',
+    value: '',
+    icon: ''
+  })
 
-  const handleActiveElement = (elem: ActiveElement) => {
-    setActiveElement(elem);
-    selectedShapeRef.current = elem?.value as string;
-  }
+  const handleActiveElement = (elem: ActiveElement) => {
+    setActiveElement(elem);
+    selectedShapeRef.current = elem?.value as string;
+  }
 
-  useEffect(() => {
-    renderCanvas({
-      fabricRef,
-      canvasObjects,
-      activeObjectRef,
-    });
-  }, [canvasObjects]);
+  useEffect(() => {
+    const canvas = initializeFabric({canvasRef, fabricRef})
+    canvas.on("mouse:down", (options) => {
+      handleCanvasMouseDown({
+        options,
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef
+      })
+    })
 
-  
+    canvas.on("mouse:move", (options) => {
+      handleCanvaseMouseMove({
+        options,
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+        syncShapeInStorage
+      })
+    })
 
-  return (
-    <main className=" h-full overflow-hidden">
-      <Navbar
-        activeElement={activeElement}
-        handleActiveElement={handleActiveElement}
-      />
+    canvas.on("mouse:up", (options) => {
+      handleCanvasMouseUp({
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+        syncShapeInStorage,
+        setActiveElement,
+        activeObjectRef,
+      })
+    })
 
-      <section className="flex h-full flex-row">
-        <LeftSidebar/>
-          <Live canvasRef={canvasRef}/>
-        <RightSidebar/>
-      </section>
-    </main>
-  );
+    window.addEventListener("resize", () => {
+      handleResize({ fabricRef })
+    })
+  }, [])
+
+  useEffect(() => {
+    renderCanvas({
+      fabricRef,
+      canvasObjects,
+      activeObjectRef
+    });
+  }, [canvasObjects]);
+
+  return (
+    <main className=" h-full overflow-hidden">
+      <Navbar
+        activeElement={activeElement}
+        handleActiveElement={handleActiveElement}
+      />
+
+      <section className="flex h-full flex-row">
+        <LeftSidebar/>
+          <Live canvasRef={canvasRef}/>
+        <RightSidebar/>
+      </section>
+    </main>
+  );
 }
-
